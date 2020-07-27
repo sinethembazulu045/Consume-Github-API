c8893d79082211108d1a613089f6a7e337421510
Sinethemba <sinethembazulu045@gmail.com>
2020-07-27 17:32:25 +0200

    latest commit

--- api.py
+++ api.py
@@ -1,10 +1,19 @@
 import requests
 import datetime as dt
+import os
+from requests.auth import HTTPBasicAuth
 
 
+GITHUB_NAME = os.environ.get('githubname')
+GITHUB_PASS = os.environ.get('git_password')
 
-def PR(repo_name, start_date, end_date):
-    pull_request = requests.get("https://api.github.com/repos/Umuzi-org/tech-department/pulls?id=453")     
-    my_data = pull_request.json()
-    return my_data
-# print(PR('Umuzi-org','1-12-2019','5-6-2020'))
+
+def PullRequest(repo_owner, repo_name, start_date, end_date):
+    url = "https://api.github.com/repos/{repo_owner}/{repo_name}/pulls" 
+    pull_request = requests.get(url, auth=HTTPBasicAuth(GITHUB_NAME, GITHUB_PASS))
+    print(pull_request.status_code)    
+    pull_request_data = pull_request.json()
+    return pull_request_data
+    
+print(PullRequest('Umuzi-org','tech-department',"2020-03-30T08:39:32Z", "2020-04-10T08:39:32Z"))
+
